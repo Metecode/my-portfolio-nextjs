43bcdad4e69d66f1c530db53611326c3db41d701
Mete Uçar <ee09mete@gmail.com>
2023-06-24 02:17:43 +0300

    hot fix5

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -8,7 +8,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 const url = "../cv.pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const resume = () => {
+const Resume = () => {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1); //setting 1 to show fisrt page
 
@@ -67,4 +67,4 @@ const resume = () => {
   );
 };
 
-export default resume;
+export default Resume;
